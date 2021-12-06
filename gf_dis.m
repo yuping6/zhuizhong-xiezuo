9a6fa2a9224ad7c8489dd77cae3e7355637602dc
“Your <your>
2021-12-06 17:16:08 +0800

    bai-v1

--- gf_dis.m
+++ gf_dis.m
@@ -1,8 +1,8 @@
 %计算GF域内两个码字的码距子程序：
 function ret = gf_dis(a,b)
 ret = 0;
-a_temp = de2bi(gf2num(a),3);
-b_temp = de2bi(gf2num(b),3);
+a_temp = de2bi(gf2num(a),2);
+b_temp = de2bi(gf2num(b),2);
 temp = a_temp+b_temp;
 temp = mod(temp,2);
 ret = sum(temp);
