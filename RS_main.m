79c3519b1b76bccb0ddad4a7f870a8f0cb3fbc4c
yuping6 <2890704667@qq.com>
2021-12-07 13:06:14 +0800

    ping1(1)

--- RS_main.m
+++ RS_main.m
@@ -8,11 +8,11 @@ code=rs_rscode(MSG)                                               %对5组信号进行
 a=zeros(5,5);
 b=floor(rand(5,2)*8);
 noise=[a,b];
-NOISE=gf(noise,3);                                                   %产生噪声信号
-in_msg=code+NOISE                                                %加噪
-for ii = 1:5                                                                  %对5组信号进行译码
+NOISE=gf(noise,3);                                                     %产生噪声信号
+in_msg=code+NOISE                                                 %加噪
+for ii = 1:5                                                                   %对5组信号进行译码
 new_msg(ii,:) = rs_rrscode(in_msg(ii,:),all_code_msg,all_msg);
 end
-new_msg                                                                    %译码输出
-num_same = same(msg,new_msg);                          %译码正确个数
-num =num_same/15                                                 %译码正确率
+new_msg                                                                     %译码输出
+num_same = same(msg,new_msg);                           %译码正确个数
+num =num_same/15                                                  %译码正确率
